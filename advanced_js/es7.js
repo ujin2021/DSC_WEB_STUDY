82537dfe0d1353800ca70ccb9153ecbfe55f7113
ujin2021 <ujin2021@naver.com>
2020-12-23 18:03:18 +0900

    ES7

---
+++ advanced_js/es7.js
@@ -0,0 +1,13 @@
+// ES6에서 추가 된 것
+// 1. includes
+console.log('Helloooo'. includes('o')) // true
+
+const pets = ['cat', 'dog', 'bat']
+console.log(pets.includes('dog')) // true
+console.log(pets.includes('birds')) // false
+
+// 2. square
+const square = (x) => x ** 2
+console.log(square(2)) // 4
+const cube = (y) => y ** 3
+console.log(cube(2)) // 8
