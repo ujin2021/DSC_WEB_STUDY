d906de0a53ea3652e3e7a33abb875fab999ae217
ujin2021 <ujin2021@naver.com>
2020-12-30 00:11:32 +0900

    express basic

--- express_basic/server.js
+++ express_basic/server.js
@@ -6,8 +6,13 @@ const app = express()
 
 app.use(bodyParser.urlencoded({extended: false})) // middleware (form)
 app.use(bodyParser.json()) // middleware (json)
+app.use(express.static(__dirname + '/public')) // static file을 띄운다(public dir안에 있는 index.html)
+
 app.get('/', (req, res) => {
-    res.send('test')
+    // console.log(req.query) // ?name=adrei&age=31 -> { name: 'adrei', age: '31' }
+    // console.log(req.header)
+    // console.log(req.params) // get('/:id'), localhost:3000/2222 => { id: '2222' }
+    res.send()
 })
 
 app.get('/profile', (req, res) => {
